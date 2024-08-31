9665087a215026287d0c2a23cce0f51e983f385f
SongDogDevLead <176419206+SongDogDevLead@users.noreply.github.com>
2024-08-31 13:39:22 -0500

    Add service worker controls

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './assets/images/bg.exr',
+    './assets/images/blackSupraComp.glb'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;  // Return the cached file
+                }
+                return fetch(event.request); // Fetch from the network if not in cache
+            })
+    );
+});
